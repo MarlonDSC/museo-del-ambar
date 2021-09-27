a59e23775858b1e3ae82ca0857a01ad453df91ef
AlbertG03 <84394953+AlbertG03@users.noreply.github.com>
2021-09-26 23:00:33 -0400

    Animación del menú desplegable.
    En esta animación el menú se desplegará y hará posible la elección de un idioma

---
+++ Assets/Animaciones del menú desplegable/Opción seleccionada/ColorSeleccionado.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ColorSeleccionado : MonoBehaviour
+{
+	public TextMeshProUGUI IdiomaSeleccionado;
+	public GameObject SeleccionIdiomaIngles;
+	public TextMeshProUGUI idiomaTextoIngles;
+	public GameObject SeleccionIdiomaEspanol;
+	public TextMeshProUGUI idiomaTextoespanol; 
+	
+
+
+	protected void Update()
+	{
+		switch (IdiomaSeleccionado.text)
+		{
+		case "ENGLISH":
+			idiomaTextoIngles.color = new Color32(247,153,12,255);
+			SeleccionIdiomaIngles.GetComponent<Image>().color = new Color32(32,32,32,255);
+			idiomaTextoespanol.color = new Color32(32,32,32,255);
+			SeleccionIdiomaEspanol.GetComponent<Image>().color = new Color32(247,153,12,255);
+			break;
+		}
+		switch (IdiomaSeleccionado.text)
+		{
+		case "ESPAÑOL":
+			idiomaTextoespanol.color = new Color32(247,153,12,255);
+			SeleccionIdiomaEspanol.GetComponent<Image>().color = new Color32(32,32,32,255);
+			idiomaTextoIngles.color = new Color32(32,32,32,255);
+			SeleccionIdiomaIngles.GetComponent<Image>().color = new Color32(247,153,12,255);
+			break;
+		}
+	}
+	public void SeleccionDecolor()
+	{
+		if(SeleccionIdiomaIngles== true){IdiomaSeleccionado.text = "ENGLISH";}
+
+	}
+	public void SeleccionDecolor2()
+	{
+		if(SeleccionIdiomaEspanol == true){IdiomaSeleccionado.text = "ESPAÑOL";}
+	
+	}
+	
+}
