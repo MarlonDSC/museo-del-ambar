d670d5e6c94e7d47dcd7f65d9d772c66fcde42df
Jhilario-dev <marlonsubuyu.2012@gmail.com>
2021-10-07 15:18:30 -0400

    Moving across panels
    MoveBetweenPanels lets you move across panels in "Boleta"

---
+++ Assets/Scripts/MoveBetweenPanels.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBetweenPanels : MonoBehaviour
+{
+	public GameObject previousPanel;
+	public GameObject nextPanel;
+
+	public void PreviousPanel(){
+		previousPanel.SetActive(true);
+		gameObject.SetActive(false);
+		
+	}
+	
+	public void NextPanel(){
+		nextPanel.SetActive(true);
+		gameObject.SetActive(false);
+	}
+	
+
+}
