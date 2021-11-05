4ed0eaef3e71729b3855c2553b1449ec46c070ae
Esteban <92889652+joshua10rey@users.noreply.github.com>
2021-11-05 14:21:25 -0400

    Animar paneles
    Creación de la UI de experiencias  y animación de la misma.

---
+++ Assets/Animacion/animaciones paneles experienciaas/animaciones menores/ReproducirAnimMee.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ReproducirAnimMee : MonoBehaviour
+{
+
+	//Contador
+	private int ParOImpar;
+	//Contadores panel menores total
+	private int ContadorBtnAmbarOrigen;
+	private int ContadorBtnJuegosOrigen;
+	//Contadores panel menores Origen
+	private int ContadorBtnAmbar;
+	private int ContadorBtnJuegos;
+	private int ContadorBtnLinterna;
+	private int ContadorBtnTabaco;
+	//Botones panel menores Origen
+	public GameObject BtnAmbarOrigen;
+	public GameObject BtnJuegosOrigen;
+	//Botones panel menores total
+	public GameObject BtnAmbar; 
+	public GameObject BtnJuegos;
+	public GameObject BtnLinterna;
+	public GameObject BtnTabaco;
+	//Animator paneles padres
+	public Animator PanelDetallesDeLaExperienciaTotalMenores;
+	public Animator PanelDetallesDeLaExperienciaOrigenMenores;
+	//Animations panel menores total
+	public AnimationClip ExperienciaAmbarOrigen;
+	public AnimationClip ExperienciaAmbarCierreOrigen;
+	public AnimationClip ExperienciaJuegosOrigen;
+	public AnimationClip ExperienciaJuegosCierreOrigen;
+	//Animations panel menores total
+	public AnimationClip ExperienciaAmbar;
+	public AnimationClip ExperienciaAmbarCierre;
+	public AnimationClip ExperienciaJuegos;
+	public AnimationClip ExperienciaJuegosCierre;
+	public AnimationClip ExperienciaLinterna;
+	public AnimationClip ExperienciaLinternaCierre;
+	public AnimationClip ExperienciaTabaco;
+	public AnimationClip ExperienciaTabacoCierre;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	
+    }
+    
+	//contadores panel experiencia origen menores    
+	public void ContadorAmbarOrigen()
+	{
+		
+		if(BtnAmbarOrigen == true )
+		{
+			ContadorBtnAmbarOrigen++;
+		
+	
+			if(ContadorBtnAmbarOrigen % 2 != 0)
+			{
+				PanelDetallesDeLaExperienciaOrigenMenores.Play(ExperienciaAmbarOrigen.name);
+				
+		
+			}
+			
+			if(ContadorBtnAmbarOrigen % 2 == 0)
+			{
+	
+				PanelDetallesDeLaExperienciaOrigenMenores.Play(ExperienciaAmbarCierreOrigen.name);		
+			}
+			
+			
+		}
+	
+	}
+	
+	public void ContadorJuegosOrigen()
+	{
+		
+		if(BtnJuegosOrigen == true )
+		{
+			ContadorBtnJuegosOrigen++;
+		
+	
+			if(ContadorBtnJuegosOrigen % 2 != 0)
+			{
+				PanelDetallesDeLaExperienciaOrigenMenores.Play(ExperienciaJuegosOrigen.name);
+				
+		
+			}
+			
+			if(ContadorBtnJuegosOrigen % 2 == 0)
+			{
+	
+				PanelDetallesDeLaExperienciaOrigenMenores.Play(ExperienciaJuegosCierreOrigen.name);		
+			}
+			
+			
+		}
+	
+	}
+	
+	
+	
+	
+	//contadores panel experiencia total menores
+	public void ContadorAmbar()
+	{
+		
+		if(BtnAmbar == true )
+		{
+			ContadorBtnAmbar++;
+		
+	
+			if(ContadorBtnAmbar % 2 != 0)
+			{
+				PanelDetallesDeLaExperienciaTotalMenores.Play(ExperienciaAmbar.name);
+				
+		
+			}
+			
+			if(ContadorBtnAmbar % 2 == 0)
+			{
+	
+				PanelDetallesDeLaExperienciaTotalMenores.Play(ExperienciaAmbarCierre.name);		
+			}
+			
+			
+		}
+	
+	}
+	
+	public void ContadorTabaco()
+	{
+		
+		if(BtnTabaco == true )
+		{
+			ContadorBtnTabaco++;
+		
+	
+			if(ContadorBtnTabaco % 2 != 0)
+			{
+				PanelDetallesDeLaExperienciaTotalMenores.Play(ExperienciaTabaco.name);
+				
+		
+			}
+			
+			if(ContadorBtnTabaco % 2 == 0)
+			{
+	
+				PanelDetallesDeLaExperienciaTotalMenores.Play(ExperienciaTabacoCierre.name);			
+			}
+
+	
+		}
+
+	}
+	
+	public void ContadorJuegos()
+	{
+		if(BtnJuegos == true )
+		{
+			ContadorBtnJuegos++;
+		
+	
+			if(ContadorBtnJuegos % 2 != 0)
+			{
+				PanelDetallesDeLaExperienciaTotalMenores.Play(ExperienciaJuegos.name);
+				
+		
+			}
+			
+			if(ContadorBtnJuegos % 2 == 0)
+			{
+	
+				PanelDetallesDeLaExperienciaTotalMenores.Play(ExperienciaJuegosCierre.name);
+
+			
+			}
+		}
+	
+	}
+	
+	public void contadorLinterna()
+	{
+	
+		if(BtnLinterna == true )
+		{
+			ContadorBtnLinterna++;
+		
+	
+			if(ContadorBtnLinterna % 2 != 0)
+			{
+				PanelDetallesDeLaExperienciaTotalMenores.Play(ExperienciaLinterna.name);
+				
+		
+			}
+			
+			if(ContadorBtnLinterna % 2 == 0)
+			{
+	
+				PanelDetallesDeLaExperienciaTotalMenores.Play(ExperienciaLinternaCierre.name);				
+			}
+				
+		}
+	}
+}
---
+++ Assets/Animacion/animaciones paneles experienciaas/animaciones menores/ScriptVR.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptVR : MonoBehaviour
+{
+	private int ParOImpar;
+	private int ContadorBtnVR;
+	public GameObject BtnVR;
+	public Animator PanelDetallesDeLaExperienciaTotalMenores;
+	public AnimationClip ExperienciaVR;
+	public AnimationClip ExperienciaVRCierre;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	public void ContadorVR()
+	{
+		if(BtnVR == true)
+		{
+			ContadorBtnVR++;
+			if(ContadorBtnVR % 2 != 0)
+			{
+				PanelDetallesDeLaExperienciaTotalMenores.Play(ExperienciaVR.name);
+				
+		
+			}
+			
+			if(ContadorBtnVR % 2 == 0)
+			{
+	
+				PanelDetallesDeLaExperienciaTotalMenores.Play(ExperienciaVRCierre.name);		
+			}
+		}
+	}
+}
